b75b102da24782594d7ab3126aaec54f38815a96
kitsuneo <polina.antipova@gmail.com>
2024-05-06 20:48:04 +0300

    catch exception if log file doesn not exist, move everything to functions

--- thisyearprogress.py
+++ thisyearprogress.py
@@ -1,7 +1,7 @@
 import calendar
 import time
 from datetime import date, datetime
-from main import *
+from bb_configs.typ_config import TYP_LOGIN, TYP_PASSWORD, LOG_FILE
 
 bot_flag = 'TYP'
 
@@ -19,28 +19,35 @@ def update_log(stroka):
         log_file.write(stroka)
 
 
-days_rem = get_current_year() - get_day_number()
 percentage_spent = round(get_day_number() / get_current_year() * 100, 2)
 
-progress_str = ''
-count = 0
 
-# TODO minor добавить переходное значение '\u2592' // \u2588 full shade
-for i in range(10):
-    if count < percentage_spent:
-        progress_str = progress_str + '\u2588'
-        count += 10
-    else:
-        progress_str = progress_str + '\u2591'
+def generate_posting(percentage: int):
+    # TODO minor добавить переходное значение '\u2592' // \u2588 full shade
+    progress_str = ''
+    count = 0
+    for i in range(10):
+        if count < percentage_spent:
+            progress_str = progress_str + '\u2588'
+            count += 10
+        else:
+            progress_str = progress_str + '\u2591'
+
+    return progress_str
+
 
 # todo cute timestamp with timezones
-s_bsky = f'{progress_str} This year progress:, {percentage_spent}% ({days_rem} days remaining)'
+s_bsky = f'{generate_posting(percentage_spent)} This year progress:, {percentage_spent}% ({get_current_year() - get_day_number()} days remaining)'
 s_log = f'{datetime.now().strftime("%Y-%m-%d, %H%M%S")}, {time.tzname[0]},{bot_flag= }, "{s_bsky}"\n'
 
 
 def get_latest_log_date(log_file):
-    with open(log_file, mode='r', encoding='utf-8') as log_file:
-        check_today = log_file.readlines()
-        last_log = check_today[-1].split(',')
-        log_flag = last_log[0]
-    return log_flag
+    try:
+        with open(log_file, mode='r', encoding='utf-8') as log_file:
+            check_today = log_file.readlines()
+            last_log = check_today[-1].split(',')
+            log_flag = last_log[0]
+        return log_flag
+    except:
+        print('no log file')
+        return None
