8f9c793b76e6dd5f2ff46ce22d97b8fc44deaf66
kitsuneo <polina.antipova@gmail.com>
2025-03-20 16:04:45 +0300

    +output beautification

--- test_typ_main.py
+++ test_typ_main.py
@@ -1,19 +1,21 @@
 from datetime import date
 
 import pytest
-from main import generate_posting_string, generate_log_string
+from typ import generate_posting_string, generate_log_string
 
 
-class TestClass:
+class TestStringClass:
     def test_posting_string(self):
-
         test_string = generate_posting_string()
-        assert type(test_string) == str
+        assert type(test_string) is str
         assert len(test_string) > 0
-        assert '\u2591' in test_string or '\u2588' in test_string
+        assert '\U0001F7E6' in test_string or '\U00002B1C' in test_string
 
     def test_logging_string(self):
         test_string = generate_log_string(True)
-        assert type(test_string) == str
+        assert type(test_string) is str
         assert len(test_string) > 0
-        assert str(date.today()) in test_string
+        assert test_string.startswith(str(date.today()))
+
+        test_string_f = generate_log_string(False)
+        assert test_string_f.startswith(str(date.today()))
--- typ.py
+++ typ.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from atproto import Client
 from bb_configs.typ_config import TYP_LOGIN, TYP_PASSWORD, LOG_FILE
 import calendar
@@ -7,7 +8,7 @@ from datetime import date, datetime
 
 # TODO create pretty config, check if config doesnt exist create the one with default settings
 # TODO Major add run by cron
-# TODO Major Add pytest
+# TODO pyproject.toml
 
 def get_current_year():
     return 366 if calendar.isleap(time.gmtime()[0]) else 365
@@ -18,26 +19,28 @@ def get_day_number():
 
 
 def generate_posting_string():
-    days_spent = round(get_day_number() / get_current_year() * 100, 2)
+    days_spent = round(get_day_number() / get_current_year() * 100)
+    print(days_spent)
     days_rem = get_current_year() - get_day_number()
-    # TODO minor '\u2592' half-full shade // \u2588 full shade
+
     progress_str = ''
     count = 0
     for i in range(10):
         if count < days_spent:
-            progress_str = progress_str + '\u2588'
+            progress_str = progress_str + '\U0001F7E6'
             count += 10
         else:
-            progress_str = progress_str + '\u2591'
+            progress_str = progress_str + '\U00002B1C'
 
-    s_bsky = f'{progress_str} This year progress: {days_spent}% ({days_rem} days remaining)'
+    s_bsky = f'[{progress_str}]\nThis year progress: {days_spent}% ({days_rem} days remaining)'
 
     return s_bsky
 
 
 def generate_log_string(posting_flag):
     status = 'ADDED' if posting_flag else 'not added'
-    s_log = f'{datetime.now().strftime("%Y-%m-%d, %H%M%S")}, {status}, {time.tzname[0]}, [{generate_posting_string()}]\n'
+    #TODO Minor make cute timezone part of the date string
+    s_log = f'{datetime.now().strftime("%Y-%m-%d, %H%M%S, %z")}, {status}, {time.tzname[0]}, {generate_posting_string()}\n'
 
     return s_log
 
@@ -62,7 +65,6 @@ def update_log(stroka):
 def main():
     """
     If latest record in log file == today, meaning the bot already posted the message, no need for other postings
-    TODO: add check on the correct log record (e.g. starting from date and not from random letters)
     """
 
     if get_latest_log_date(LOG_FILE) == str(date.today()):
